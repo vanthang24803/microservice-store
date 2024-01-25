8e5d2cb7e334dd17e7551af3b1c430893ee73328
vanthang24803 <nguyenthang24803@gmail.com>
2024-01-25 20:15:18 +0700

    Add Readme.md

--- Order/obj/Debug/net8.0/Order.AssemblyInfo.cs
+++ Order/obj/Debug/net8.0/Order.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Order")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35e2af8110964f41b04b1d32ccc5ab379dd65eba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47c8a75f69bc3f2a2b56ddb145b29c392a2e3634")]
 [assembly: System.Reflection.AssemblyProductAttribute("Order")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Order")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
