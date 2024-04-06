3fe7ef6662a55e99cd3e2e3a79cf840f8a9d5aa2
unknown <nguyenthang240803@gmail.com>
2024-04-06 09:37:55 +0700

    fix docker file and Blog route

--- Product/obj/Debug/net8.0/Product.AssemblyInfo.cs
+++ Product/obj/Debug/net8.0/Product.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Product")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3412806a1a789740ae47f4822750ce77732eef05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07f3e738f577172bbc0fcf9487155317359f1732")]
 [assembly: System.Reflection.AssemblyProductAttribute("Product")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Product")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
