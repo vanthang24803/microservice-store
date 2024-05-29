c582228e190f6d0ff5067a2b90f1aa33b7e970f5
unknown <nguyenthang240803@gmail.com>
2024-05-29 20:16:39 +0700

    Update Dependency

--- Product/obj/Debug/net8.0/Product.AssemblyInfo.cs
+++ Product/obj/Debug/net8.0/Product.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Product")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf05c8899f8a08991dbbbf81ce385d5605e1e8c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+187d4af4ecd1e755970d7a74d6a148390ed05489")]
 [assembly: System.Reflection.AssemblyProductAttribute("Product")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Product")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
